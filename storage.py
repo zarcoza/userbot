83486ee88fd3d635c16a7ebbe8e0c82734da1146
Zara <azaleazaragoza@gmail.com>
2025-05-24 09:45:44 +0700

    storage.py

--- storage.py
+++ storage.py
@@ -1,7 +1,18 @@
-# storage.py
 import json
 import os
-from config import PRESET_FILE
+
+JOB_FILE = "storage/job_storage.json"
+PRESET_FILE = "storage/preset_storage.json"
+
+def load_jobs():
+    if not os.path.exists(JOB_FILE):
+        return {}
+    with open(JOB_FILE, "r") as f:
+        return json.load(f)
+
+def save_jobs(jobs):
+    with open(JOB_FILE, "w") as f:
+        json.dump(jobs, f, indent=2)
 
 def load_presets():
     if not os.path.exists(PRESET_FILE):
